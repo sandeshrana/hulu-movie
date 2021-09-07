86b791ca7b505f447b9c87537826ae2603303b11
sandeshrana <sandeshrana2178@gmail.com>
2021-09-07 18:41:34 +0545

    ERROR 32

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -40,7 +40,17 @@ export default function PhotoGallery() {
         <div className='md:flex'>
           <div className='flex-1 text-gray-700 text-center-700 bg-gray-400 px-5 py-5 m-2 rounded'>
             <div className='lg:flex lg:item-center'>
-              <div className='lg:flex-shrink-0'></div>
+              <div className='lg:flex-shrink-0'>
+                <img className='rounded-lg  lg:w-64' src="https://source.unsplash.com/1600*900/?nature,water" alt="ERROR" />
+              </div>
+              <div className="mt-4 lg:mt-0 ml-6">
+                <div className='uppercase tracking-wide text-sm text-indigo-600 font-bold'>
+                  Networking
+                </div>
+                <a href="#"
+                  className='block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline'
+                >Finding connection to help your business grow</a>
+              </div>
             </div>
           </div>
         </div>
