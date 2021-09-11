b60ef2a88084ceb8cfb335f251672236e55dad76
sandeshrana <sandeshrana2178@gmail.com>
2021-09-11 22:26:06 +0545

    praticee

--- src/components/Making.jsx
+++ src/components/Making.jsx
@@ -1,6 +1,6 @@
 
 import Link from 'next/link'
-
+import { IoLocationOutline } from '<react-icons/io'
 //main fn()
 export default function Making() {
   //your variable, function and hooks go here
@@ -10,18 +10,20 @@ export default function Making() {
     <>
       {/* Code Starts from here */}
       <div className="flex bg-black ">
-        <div className="text-white w-1/12 text-lg text-center font-bold">Amazon</div>
-        <div className="text-white w-1/12">Deliver To Nepal</div>
+        <div className="text-white w-1/12 text-2xl text-center font-bold my-auto">amazon</div>
+        <div className="text-white w-1/12 text-sm max-w-max text-center">Deliver to <IoLocationOutline /> <span className='font-bold text-lg' > Nepal</span></div>
         <div className="text-white w-6/12">
-          <span className='bg-gray-100 text-black font-bold text-lg mr-5 p-2'>ALL <input className='border-2 text-black ' type="search" placeholder name="Search" id="" />🔍</span>
+          <span className='bg-gray-100 text-black  text-lg mr-5 p-2 text-sm'>All <input className='border-2 text-black ' type="search" placeholder name="Search" id="" />🔍</span>
         </div>
-        <div className="text-white w-1/12">Flag🇳🇵</div>
-        <div className="text-white w-1/12">Account</div>
-        <div className="text-white w-1/12">Order</div>
-        <div className="text-white w-1/12">Cart</div>
+        <div className="text-white w-1/12 font-bold my-auto">Flag 🇳🇵</div>
+        <div className="text-white w-1/12 text-sm my-auto ">Hello,Sing in <span className='font-bold'>
+          Account & List</span></div>
+        <div className="text-white w-1/12 text-sm font-bold my-auto"> Return &
+          Order</div>
+        <div className="text-white w-1/12 font-bold text-sm my-auto text-center ">Cart</div>
       </div>
       <div className="flex bg-gray-700 text-white ">
-        <div className="mx-3">ALL</div>
+        <div className="mx-3 text-sm p-1 font-bold">☰ All</div>
         <div className="mx-3">Today's Deals</div>
         <div className="mx-3">Customer Service</div>
         <div className="mx-3">Registry</div>
